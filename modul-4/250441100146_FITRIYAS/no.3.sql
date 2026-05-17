37b2c25d030ffa657a2d669e5aa4cb82ff093571
fitritriyas440-tech <fitritriyas440@gmail.com>
2026-05-17 21:39:41 +0700

    250441100146_Fitriyas_MODUL 4_Kak Salman

---
+++ modul-4/250441100146_FITRIYAS/no.3.sql
@@ -0,0 +1,6 @@
+SELECT m.nama_mahasiswa, mk.nama_mk, k.semester_ambil
+FROM krs k
+JOIN mahasiswa m ON k.id_mahasiswa = m.id_mahasiswa
+JOIN mata_kuliah mk ON k.id_mk = mk.id_mk
+WHERE k.semester_ambil BETWEEN 3 AND 5
+ORDER BY k.semester_ambil ASC;
